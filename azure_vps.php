7b880d7736f0e31520db7f63ee3511ea86298735
RISA RISKI AMALIA - <19102079@ittelkom-pwt.ac.id>
2021-12-12 18:30:25 +0700

    first commit

---
+++ azure_vps.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Risa Riski Amalia";
+$NIM = "19102079";
+
+?>
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta htt-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Belajar VPS dengan Azure</title>
+</head>
+<body>
+    <h1>Belajar VPS dengan Azure</h1>
+    <p><?php echo $name ?></p>
+    <p><?php echo $NIM ?></p>
+</body>
+</html>
